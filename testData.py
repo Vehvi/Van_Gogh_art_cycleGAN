a4b25cd1f162204561cc6be823db47ed3831b2df
vehvi <anttivehvilainen123@gmail.com>
2025-10-22 12:42:00 +0300

    fix to comment

--- testData.py
+++ testData.py
@@ -2,7 +2,7 @@ from data import get_dataloaders
 
 # Simple test to verify dataloader functionality
 if __name__ == "__main__":
-    loader_A, loader_B = get_dataloaders(num_workers=0)  # tai pidä 2, mutta tämä helpottaa testissä
+    loader_A, loader_B = get_dataloaders(num_workers=0) # Set num_workers=0 for Windows compatibility
     for imgs, labels in loader_A:
         print(imgs.shape)
         break
